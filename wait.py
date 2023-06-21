47d7652a8bdfee092ac78d46cde44fea268bde5f
Jefferson Braga <jeffbraga@gmail.com>
2023-06-21 10:09:44 -0300

    Feat: Add new function

--- wait.py
+++ wait.py
@@ -44,18 +44,21 @@ def get_images_by_app(api_prefix, app_uuid, correct_image):
 
 def get_images_by_schedulers(api_prefix, app_uuid, correct_image):
     result = []
-    for instance_uuid in get_instances(api_prefix, app_uuid):
-        result += get_images_by_instance(api_prefix, instance_uuid, correct_image)
+    response = requests.get(f"{api_prefix}/schedulers/{instance_uuid}",
+                            headers={"accept": "application/json"})
+    result = json.loads(response.content)
+    print(f" - result: {result}", flush=True)
     return result
 
-def deploy_finished(api_prefix, app_id, correct_image):
-    images = get_images_by_app(api_prefix, app_id, correct_image)
+
+def deploy_finished(api_prefix, app_id, correct_image, type_url):
+    images = get_images_by_app(api_prefix, app_id, correct_image) if type_url == 'apps' else get_images_by_schedulers(api_prefix, app_id, correct_image)
     print(f" - images: {images}", flush=True)
     counted = Counter(images)
     return len(counted) == 1 and counted.get(correct_image) is not None
 
 
-def wait_deploy_finished(api_prefix, app_uuid, correct_image, timeout):
+def wait_deploy_finished(api_prefix, app_uuid, correct_image, timeout, type_url):
     while True:
         try:
             if deploy_finished(api_prefix, app_uuid, correct_image):
@@ -84,11 +87,13 @@ if __name__ == "__main__":
     splitted = re.split(r'/(apps|schedulers)/',cops_url)
     app_uuid = splitted[-1].split('/')[0]
     api_prefix = splitted[0]
+    type_url = cops_url.split('/')[4]
 
     print(f"Waiting deploy to finish", flush=True)
     print(f" - api_prefix: {api_prefix}", flush=True)
     print(f" - app_uuid: {app_uuid}", flush=True)
     print(f" - timeout: {timeout} seconds", flush=True)
     print(f" - correct_image: {correct_image}", flush=True)
+    print(f" - type: {type_url}", flush=True)
 
-    wait_deploy_finished(api_prefix, app_uuid, correct_image, timeout)
+    wait_deploy_finished(api_prefix, app_uuid, correct_image, timeout, type_url)
